6f41ee71daefec3793f5614017a52614f172ae39
Yanick Mviena <mvienayann@gmail.com>
2019-09-12 15:57:49 -0400

    user activity

--- app/src/main/java/com/example/composeemail/MainActivity.java
+++ app/src/main/java/com/example/composeemail/MainActivity.java
@@ -1,6 +1,24 @@
 package com.example.composeemail;
 
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+       setContentView(R.layout.activitymain);
+    }
+
+
+
+
+
+
+
 }
